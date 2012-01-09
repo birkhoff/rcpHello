112180998e975eba774ce780e0d8f384aff7bbcf
birkhoff <mike.birkhoff@gmx.de>
2012-01-09 13:50:32 +0100

    new RCP Plugin added

---
+++ de.birkhoff.rcp.sample.plugin/src/de/birkhoff/rcp/sample/plugin/ApplicationWorkbenchAdvisor.java
@@ -0,0 +1,20 @@
+package de.birkhoff.rcp.sample.plugin;
+
+import org.eclipse.ui.application.IWorkbenchWindowConfigurer;
+import org.eclipse.ui.application.WorkbenchAdvisor;
+import org.eclipse.ui.application.WorkbenchWindowAdvisor;
+
+public class ApplicationWorkbenchAdvisor extends WorkbenchAdvisor {
+
+	private static final String PERSPECTIVE_ID = "de.birkhoff.rcp.sample.plugin.perspective";
+
+	public WorkbenchWindowAdvisor createWorkbenchWindowAdvisor(
+			IWorkbenchWindowConfigurer configurer) {
+		return new ApplicationWorkbenchWindowAdvisor(configurer);
+	}
+
+	public String getInitialWindowPerspectiveId() {
+		return PERSPECTIVE_ID;
+	}
+
+}
